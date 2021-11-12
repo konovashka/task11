efe3e52c401979fa43d1ec87ce58918e8192eeb4
konovashka <anton_konovalov_1@mail.ru>
2021-11-12 21:40:58 +0300

    Добавьте файлы проекта.

---
+++ task11/task11.cpp
@@ -0,0 +1,37 @@
+﻿// Размен местами минимального и максимального элемента массива
+
+#include <iostream>
+
+int main()
+{
+    const int n = 5;
+    int a[n];
+	int b,numbMin,numbMax,max,min;
+	max = INT_MIN;
+	min = INT_MAX;
+	numbMax = numbMin = 0;
+	for (int i = 0; i < n; i++)
+	{
+		scanf_s("%d", &a[i]);
+	}
+	for (int i = 0; i < n; i++)
+	{
+		if (a[i]>max)
+		{
+			max = a[i];
+			numbMax = i;
+		}
+		if (a[i]<min)
+		{
+			min = a[i];
+			numbMin = i;
+		}
+	}
+	b = a[numbMin];
+	a[numbMin] = a[numbMax];
+	a[numbMax] = b;
+	for (int i = 0; i < n; i++)
+	{
+		printf("%d ", a[i]);
+	}
+}
